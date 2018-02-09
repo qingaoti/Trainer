46452f3115997563380e3a28500b8473f68b216c
gtqin <gtqin@cheweilai.cn>
2018-02-09 09:29:13 +0800

    提交测试代码

---
+++ JStest/random.js
@@ -0,0 +1,3 @@
+console.log(Math.round(Math.random() * 6) + 3);
+console.log((Math.round(Math.random() * 10) % 2)===0 ? 'M' : 'F');
+console.log((Math.round(Math.random() * 10) % 2)===0 ? true : false);
---
+++ JStest/str_test.js
@@ -0,0 +1,3 @@
+var mctName = "桂林金优胜贸易有限公司（力帆）";
+var _mctName = mctName.replace(/\(|\)|\（|\）/gi, '.');
+console.log(new RegExp(_mctName));
